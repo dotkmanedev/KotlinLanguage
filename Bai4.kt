92a382ecd0bfd0731def28b5a63f75d3a7120d57
ducmanhtechdev <supermoffcial@gmail.com>
2026-03-06 12:57:27 +0700

    1234

---
+++ Bai4.kt
@@ -0,0 +1,31 @@
+fun main() {
+//    var i1 = 2;
+//    var i2 = 5;
+//    var i3 = -3;var d1 = 2.0;
+//    var d2 = 5.0;
+//    var d3 = -0.5;
+
+
+    // bai 1;
+//    var result1 = i1 + (i2 * i3);
+//    var result2 = i1 * (i2 + i3);
+//    var result3 = i1.div(i3 + i2);
+//    var result4 = (i1.toDouble() / i2.toDouble()) + i3;
+//    var result5 = 3 + 4 + (5 / 3).toDouble();
+//    var result6 = (3 + 4 + 5) / 3;
+//    var a = 3;
+//   var b = a.unaryMinus();
+//    print("$b");
+    // bai 2;
+//    var a = 1;
+//    var b = 1;
+//    if (a == b) {
+//        print(a);
+//    }
+// bai3
+
+//    var x = 6;
+//    print(x::class.java.typeName);
+
+
+}
